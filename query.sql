2cabc16bcff9a39c5395e8825367f443552c37be
haris.saputra.aux <haris.saputra.aux@flip.id>
2025-04-22 14:38:42 +0700

    sql query test

---
+++ query.sql
@@ -0,0 +1,22 @@
+select
+	m.id as id_murid,
+	m.name,
+	p.status,
+	m.time_create,
+	p.time_create as time_update
+from
+	murid m
+left join
+	(
+	select
+		id_murid,
+		status,
+		time_create
+	from
+		pendidikan p
+	where time_create = (
+		select max(time_create)
+		from pendidikan p2
+		where p2.id_murid = p.id_murid
+	)
+) as p on p.id_murid = m.id
